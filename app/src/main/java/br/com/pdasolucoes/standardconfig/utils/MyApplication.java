cf2173b7936180c184df63bb44091e3b57a23862
viniciusnovais <v.novais@outlook.com>
2025-11-12 17:25:42 -0300

    teste new

--- app/src/main/java/br/com/pdasolucoes/standardconfig/utils/MyApplication.java
+++ app/src/main/java/br/com/pdasolucoes/standardconfig/utils/MyApplication.java
@@ -15,7 +15,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.multidex.MultiDex;
 
 import br.com.pdasolucoes.standardconfig.R;
 import br.com.pdasolucoes.standardconfig.managers.AuthManager;
@@ -40,7 +39,7 @@ public class MyApplication extends Application implements DialogInterface.OnShow
         MyApplication.instance = this;
         super.onCreate();
 
-        MultiDex.install(this);
+        //MultiDex.install(this);
 
         registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
             @Override
